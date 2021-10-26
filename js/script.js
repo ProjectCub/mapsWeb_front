4662b6375f396c80e4a68887fc67bb2622fc97c0
inciner8r <punarvpawade17@gmail.com>
2021-10-27 00:05:05 +0530

    record latitude and longitude

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
-console.log("kek")
-function initMap(){
+var data = {
+  sender: null,
+  timestamp: null,
+  lat: null,
+  lng: null
+};
+
+function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 2,
-  })
+    zoom: 5,
+    center: {lat: 0, lng: 0}  });
+    map.addListener('click', function(e) {
+      data.lat = e.latLng.lat();
+      data.lng = e.latLng.lng();
+      console.log(data.lat,data.lng);
+    });
 }
+
+
