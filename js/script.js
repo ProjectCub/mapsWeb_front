b578adbf1f88a4694c3e07eabcc624d3f0bdaecc
inciner8r <punarvpawade17@gmail.com>
2021-10-27 00:24:08 +0530

    temp amend later

--- js/script.js
+++ js/script.js
@@ -12,8 +12,31 @@ function initMap() {
     map.addListener('click', function(e) {
       data.lat = e.latLng.lat();
       data.lng = e.latLng.lng();
+      const myLatlng = { lat: data.lat, lng: data.lng };
       console.log(data.lat,data.lng);
+      const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 5,
+        center: myLatlng  });
+      
+        const marker = new google.maps.Marker({
+          position: myLatlng,
+          map,
+          title: "Click to zoom",
+        });
+    
+      map.addListener("center_changed", () => {
+        // 3 seconds after the center of the map has changed, pan back to the
+        // marker.
+        window.setTimeout(() => {
+          map.panTo(marker.getPosition());
+        }, 3000);
+      });
+      marker.addListener("click", () => {
+        map.setZoom(15);
+        map.setCenter(marker.getPosition());
+      });
     });
+
 }
 
 
