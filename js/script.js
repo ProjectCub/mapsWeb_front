6aa1501ba3252ff3d43734dcf40aee57772b72ae
vaibhav <vaibhavng7@gmail.com>
2021-10-29 00:35:24 +0530

    Added marker on click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-console.log("kek")
-function initMap(){
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 2,
-  })
-}
+document.addEventListener(
+    "DOMContentLoaded",
+    () => {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiaW5jaW5lcjhyIiwiYSI6ImNrdjl2eWhoeDBhcTkycW1neXIwNTF4bGYifQ.D_EYYNYwxr4ejAGqBkrqcw';
+        const map = new mapboxgl.Map({
+            container: 'map', // container ID
+            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            center: [-74.5, 40], // starting position [lng, lat]
+            zoom: 9 // starting zoom
+        });
+        let marker = null;
+        map.on('click', (e) => {
+            if (marker != null) {
+                marker.remove();
+            }
+
+             marker = new mapboxgl.Marker()
+                .setLngLat(e.lngLat)
+                .addTo(map); // add the marker to the map
+        });
+    },
+
+    false
+)
