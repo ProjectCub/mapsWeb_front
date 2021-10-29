902e60776c06a188652299c52433aae0b65ca2ab
inciner8r <punarvpawade17@gmail.com>
2021-10-29 22:30:03 +0530

    autoformat

--- js/script.js
+++ js/script.js
@@ -1,24 +1,21 @@
 document.addEventListener(
-    "DOMContentLoaded",
-    () => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiaW5jaW5lcjhyIiwiYSI6ImNrdjl2eWhoeDBhcTkycW1neXIwNTF4bGYifQ.D_EYYNYwxr4ejAGqBkrqcw';
-        const map = new mapboxgl.Map({
-            container: 'map', // container ID
-            style: 'mapbox://styles/mapbox/streets-v11', // style URL
-            center: [-74.5, 40], // starting position [lng, lat]
-            zoom: 9 // starting zoom
-        });
-        let marker = null;
-        map.on('click', (e) => {
-            if (marker != null) {
-                marker.remove();
-            }
-
-             marker = new mapboxgl.Marker()
-                .setLngLat(e.lngLat)
-                .addTo(map); // add the marker to the map
-        });
-    },
-
-    false
-)
+  "DOMContentLoaded",
+  () => {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoiaW5jaW5lcjhyIiwiYSI6ImNrdjl2eWhoeDBhcTkycW1neXIwNTF4bGYifQ.D_EYYNYwxr4ejAGqBkrqcw";
+    const map = new mapboxgl.Map({
+      container: "map", // container ID
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+    let marker = null;
+    map.on("click", (e) => {
+      if (marker != null) {
+        marker.remove();
+      }
+      marker = new mapboxgl.Marker().setLngLat(e.lngLat).addTo(map); // add the marker to the map
+    });
+  },
+  false
+);
